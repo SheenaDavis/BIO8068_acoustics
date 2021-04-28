aa57b75fd063c07f19636c0b9b9acbe37a942e3f
SheenaDavis <83217449+SheenaDavis@users.noreply.github.com>
2021-04-28 11:49:13 +0100

    Start of bioacoustics script

---
+++ bioacoustics.R
@@ -0,0 +1,8 @@
+install.packages("devtools")
+devtools::install_github("https://github.com/DenaJGibbon/behaviouR")
+
+library(behaviouR)
+library(tuneR)
+library(seewave)
+library(ggplot2)
+library(dplyr)
